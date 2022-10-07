8c0755ee1215de80e5637782360edddf1600de16
Daniel Kistemacher <danielkistemacher@gmail.com>
2022-10-07 15:55:00 -0300

    update

--- api/app/users/index.js
+++ api/app/users/index.js
@@ -1,4 +1,6 @@
 import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
@@ -20,5 +22,39 @@ export const create = async ctx => {
     } catch (error) {
         ctx.body = error
         ctx.status = 500 
+    }
+}
+
+export const login = async ctx => {
+    const [type, token] = ctx.headers.authorization.split(" ")
+    const [email, plainTextPassword] = atob(token).split(":")
+    
+    const user = await prisma.user.findUnique({
+        where: { email }
+    })
+
+    if (!user) {
+        ctx.status = 404
+        return
+    }
+
+    const passwordMatch = await bcrypt.compare(plainTextPassword, user.password)
+
+    if (!passwordMatch){
+        ctx.status = 404
+        return
+    }
+
+    const { password, ...result } = user
+
+    const accessToken = jwt.sign({
+        sub: user.id,
+        nome: user.name,
+        expiresIn: "7d"
+    }, process.env.JWT_SECRET)
+
+    ctx.body = {
+        user: result,
+        accessToken
     }
 }
