7ea3c1072cd3068af6be0580aa4090c563c53101
Daniel Kistemacher <danielkistemacher@gmail.com>
2022-10-08 10:30:29 -0300

    update

--- web/src/pages/Dashboard/index.jsx
+++ web/src/pages/Dashboard/index.jsx
@@ -1,14 +1,35 @@
+import { useState, useEffect} from 'react'
 import { Navigate } from 'react-router-dom'
-import { useLocalStorage } from 'react-use'
+import { useLocalStorage, useAsyncFn } from 'react-use'
+import axios from 'axios'
+import { format } from 'date-fns'
 
 import {Icon, Card, DateSelect} from '~/components'
 
 export const Dashboard = () => {
+        const [currentDate, setDate] = useState('2022-11-20T00:00:00Z')
         const [auth] = useLocalStorage('auth', {})
+
+        const [state, doFetch] = useAsyncFn(async (params) => {
+            const res = await axios({
+                method: 'get',
+                baseURL: 'http://localhost:3000',
+                url: '/games',
+                params
+            })
+
+            return res.data
+        })
+
+        useEffect(() => {
+            doFetch({ gameTime: currentDate })
+        }, [currentDate])
 
         if(!auth?.user?.id) {
             return <Navigate to="/" repalce={true} />
           }
+
+        
 
         return (
         <>
@@ -32,26 +53,19 @@ export const Dashboard = () => {
 
                 <section id="content" className="container max-w-3xl p-4 space-y-4">
 
-                <DateSelect />
+                <DateSelect currentDate={currentDate} onChange={setDate} />
 
                     <div className="space-y-4">
-                        <Card 
-                            timeA={{slug: 'sui'}}
-                            timeB={{slug: 'cam'}}
-                            match={{time: '7:00'}}
-                        />
+                        {state.loading && 'Carregando jogos.....'}
+                        {state.error && 'Ops! Algo deu errado.'}
 
-                        <Card 
-                            timeA={{slug: 'uru'}}
-                            timeB={{slug: 'cor'}}
-                            match={{time: '7:00'}}
-                        />
-
-                        <Card 
-                            timeA={{slug: 'por'}}
-                            timeB={{slug: 'gan'}}
-                            match={{time: '7:00'}}
-                        />
+                        {!state.loading && !state.error && state.value?.map(game => (
+                            <Card 
+                                homeTeam={{slug: game.homeTeam}}
+                                awayTeam={{slug: game.awayTeam}}
+                                match={{time: format(new Date(game.gameTime), 'H:mm') }}
+                            />
+                        ))}
                     </div>
                 </section>
             </main>
--- web/src/pages/Profile/index.jsx
+++ web/src/pages/Profile/index.jsx
@@ -1,52 +1,68 @@
+import { useLocalStorage } from 'react-use'
+import { Navigate } from 'react-router-dom'
+
 import {Icon, Card, DateSelect} from '~/components'
 
-export const Profile = () => (
-    <>
+export const Profile = () => {
+    const [auth, setAuth] = useLocalStorage('auth', {})
 
-        <header className="bg-red-500 text-white">
-            <div className="container max-w-3xl flex justify-between p-4">
-                <img src="/imgs/logo-red.svg" className="w-28 md:w-40"/>
-            </div>
-        </header>
+    const logout = () => setAuth({})
 
-        <main className="space-y-6">
-            <section id="header" className="bg-red-500 text-white">
-                <div className="container max-w-3xl space-y-2 p-4">
-                    <a href="/dashboard">
-                        <Icon name="back" className="w-10"/>
-                    </a>
-                    <h3 className="text-2xl font-bold">Bruno Bertolini</h3>
+    if(!auth?.user?.id) {
+        return <Navigate to="/" repalce={true} />
+      }
+
+    return (
+        <>
+
+            <header className="bg-red-500 text-white">
+                <div className="container max-w-3xl flex justify-between p-4">
+                    <img src="/imgs/logo-red.svg" className="w-28 md:w-40"/>
+                    <div onClick={logout} className="p-2 cursor-pointer">
+                        Sair
+                    </div>
                 </div>
-            </section>
+            </header>
 
-            <section id="content" className="container max-w-3xl p-4 space-y-4">
+            <main className="space-y-6">
+                <section id="header" className="bg-red-500 text-white">
+                    <div className="container max-w-3xl space-y-2 p-4">
+                        <a href="/dashboard">
+                            <Icon name="back" className="w-10"/>
+                        </a>
+                        <h3 className="text-2xl font-bold">Bruno Bertolini</h3>
+                    </div>
+                </section>
 
-                <h2 className="text-red-500 text-xl font-bold">
-                    Seus palpites
-                </h2>
+                <section id="content" className="container max-w-3xl p-4 space-y-4">
 
-                <DateSelect />
+                    <h2 className="text-red-500 text-xl font-bold">
+                        Seus palpites
+                    </h2>
 
-                <div className="space-y-4">
-                    <Card 
-                        timeA={{slug: 'sui'}}
-                        timeB={{slug: 'cam'}}
-                        match={{time: '7:00'}}
-                    />
+                    <DateSelect />
 
-                    <Card 
-                        timeA={{slug: 'uru'}}
-                        timeB={{slug: 'cor'}}
-                        match={{time: '7:00'}}
-                    />
+                    <div className="space-y-4">
+                        <Card 
+                            timeA={{slug: 'sui'}}
+                            timeB={{slug: 'cam'}}
+                            match={{time: '7:00'}}
+                        />
 
-                    <Card 
-                        timeA={{slug: 'por'}}
-                        timeB={{slug: 'gan'}}
-                        match={{time: '7:00'}}
-                    />
-                </div>
-            </section>
-        </main>
-    </>   
-)
+                        <Card 
+                            timeA={{slug: 'uru'}}
+                            timeB={{slug: 'cor'}}
+                            match={{time: '7:00'}}
+                        />
+
+                        <Card 
+                            timeA={{slug: 'por'}}
+                            timeB={{slug: 'gan'}}
+                            match={{time: '7:00'}}
+                        />
+                    </div>
+                </section>
+            </main>
+        </>   
+    )
+}
